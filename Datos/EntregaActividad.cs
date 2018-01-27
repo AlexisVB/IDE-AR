12931b117dd21d7c9d1b10f8c5149e87bd589224
Alexis Daniel Villicaña Barrera <34362737+AlexisVB@users.noreply.github.com>
2018-01-27 01:51:32 -0600

    Add files via upload

---
+++ Datos/EntregaActividad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDE_AR.Datos
+{
+    class EntregaActividad
+    {
+        private int idActividad;
+        private string fechaEntregada;
+        private List<string> direccionesArchivos=new List<string>();
+        private int calificacion;
+        private string comentarios;
+
+        public int Idactividad
+        {
+            get{return idActividad;}
+            set{idActividad=value;}
+        }
+        public string FechaEntergada
+        {
+            get{return fechaEntregada;}
+            set{fechaEntregada=value;}
+        }
+        public List<string> DireccionesArchivos
+        {
+            get{return direccionesArchivos;}
+            set{direccionesArchivos=value;}
+        }
+        private int Calificacion
+        {
+            get{return calificacion;}
+            set{calificacion=value;}
+        }
+        private string Comentarios
+        {
+            get{return comentarios;}
+            set{comentarios=value;}
+        }
+    }
+}
---
+++ Datos/IntegrantesGrupo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDE_AR.Datos
+{
+    class IntegrantesGrupo
+    {
+        private grupo gpo;
+        private int[] idIntegrantes;
+        private int numIntegrantes;
+        List<usuario> integrantesLista=new List<usuario>();
+       
+        public IntegrantesGrupo(int numeroIntegrantes)
+        {
+            numIntegrantes = numeroIntegrantes;
+            idIntegrantes = new int[numeroIntegrantes];
+        }
+        public grupo Gpo
+        {
+            get { return gpo; }
+            set { gpo = value; }
+        }
+        public int[] IdIntegrantes
+        {
+            get{ return idIntegrantes;}
+            set { idIntegrantes = value; }
+        }
+        public List<usuario> IntegrantesLista
+        {
+            get { return integrantesLista; }
+            set { integrantesLista = value; }
+        }
+        //funcion que obtiene la lista de los usuarios en un grupo recibiendo los ids a buscar
+        public int ObtenerIntegrantesGrupo(int[] idInte)
+        {
+            idIntegrantes = idInte;
+            for (int i = 0; i < numIntegrantes; i++)
+            {
+                usuario nuevo = usuario.ObtenerUsuario(idIntegrantes[i]);
+                if (nuevo != null)
+                {
+                    integrantesLista.Add(nuevo);
+                }
+            }
+            return integrantesLista.Count;
+        }
+        //funcion que obtiene la lista de los usuarios en un grupo
+        public int ObtenerIntegrantesGrupo()
+        {
+            for(int i=0;i<numIntegrantes;i++)
+            {
+                usuario nuevo=usuario.ObtenerUsuario(idIntegrantes[i]);
+                if(nuevo!=null)
+                {
+                    integrantesLista.Add(nuevo);
+                }
+            }
+            return integrantesLista.Count;
+        }
+    }
+}
