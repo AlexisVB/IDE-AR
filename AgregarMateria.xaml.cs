59c2d7fa5e804a0d2dd66a8f84fee004aab3a2c7
Alexis Daniel Villicaña Barrera <34362737+AlexisVB@users.noreply.github.com>
2018-01-27 01:47:06 -0600

    Archivos faltantes

---
+++ AgregarMateria.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using IDE_AR.Datos;
+namespace IDE_AR
+{
+    /// <summary>
+    /// Interaction logic for AgregarMateria.xaml
+    /// </summary>
+    public partial class AgregarMateria : Window
+    {
+        public materia nuevaMateria = new materia();
+        public AgregarMateria()
+        {
+            InitializeComponent();
+            btnColor.DataContext = nuevaMateria;
+            btnPreview.DataContext = nuevaMateria;
+            nuevaMateria.Color = "#444";
+            
+        }
+        private void txtNombreMateria_TextChanged(Object sender,RoutedEventArgs e)
+        {
+            btnPreview.DataContext = null;
+            nuevaMateria.NombreMateria = txtNombreMateria.Text;            
+            btnPreview.DataContext = nuevaMateria;
+            
+        }
+        private void txtMatriculaMateria_TextChanged(object sender, TextChangedEventArgs e)
+        {            
+            nuevaMateria.Matricula = txtMatriculaMateria.Text;            
+        }
+      
+        private void txtNickMateria_TextChanged(Object sender, RoutedEventArgs e)
+        {
+
+            
+            if(txtNickMateria.Text.Length==2)
+            {
+                btnPreview.DataContext = null;
+                nuevaMateria.Nick = txtNickMateria.Text.ToUpper();
+                btnPreview.DataContext = nuevaMateria;            
+            }
+            
+        }
+        private void btnColor_Click(Object sender,RoutedEventArgs e)
+        {
+            btnPreview.DataContext = null;
+            btnColor.DataContext = null;
+            nuevaMateria.Color = "#2979ff";
+            btnPreview.DataContext = nuevaMateria;
+            btnColor.DataContext = nuevaMateria;
+        }
+        private void btnCerrar_Click(Object sender,RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
