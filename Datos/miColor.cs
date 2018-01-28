8d0442f7ec3c52ec6e6265432508ca717cae92b3
AlexisVB <34362737+AlexisVB@users.noreply.github.com>
2018-01-27 18:35:01 -0600

    Resolví los .cs ignorados

---
+++ Datos/miColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDE_AR.Datos
+{
+    public class miColor
+    {
+        private string colorName;
+        private string colorHex;
+
+        public string ColorName
+        {
+            get { return colorName; }
+            set { colorName = value; }
+        }
+        public string ColorHex
+        {
+            get { return colorName; }
+            set { colorName = value; }
+        }
+    }
+}
